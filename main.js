8f7c9def5415df55b4b279bd4002dad694727f61
Pauline <pauline.panjo@outlook.com>
2020-12-22 21:35:51 -0500

    update message function and word banks

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-const verbs1 = ['are', 'can'];
+const verbs1 = ['are', 'can', 'love'];
 const adjs = ['beautiful', 'strong', 'smart', 'confident', 'bold', 'amazing', 'powerful'];
-const nouns = ['winner', 'boss', 'genius', 'professional'];
+const verbs2 = ['win', 'do it', 'succeed'];
 
 const runAffirmation = () => {
     let affirmation = ['You'];
     let randoNum = wordType => {
         return Math.floor(Math.random() * wordType.length);
     }
-    console.log(randoNum(adjs));
-    affirmation.push(verbs1[randoNum(verbs1)]);
-    affirmation.push(adjs[randoNum(adjs)]);
-    return affirmation.join(' ');
+    let verb = verbs1[randoNum(verbs1)];
+    //console.log(randoNum(adjs));
+    if (verb === 'are') {
+    affirmation.push(verb);
+    affirmation.push(`${adjs[randoNum(adjs)]}!`);
+    } else if (verb === 'can'){
+    affirmation.push(verb);
+    affirmation.push(`${verbs2[randoNum(verbs2)]}!`);
+    } else if (verb === 'love') {
+    affirmation.push(verb);
+    affirmation.push('being you!');
+    }
+    console.log(affirmation.join(' '));
 }
 
 runAffirmation();
